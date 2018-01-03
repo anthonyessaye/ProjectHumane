20ca392ad30e0ec0d0a2ca59fa7c8bf5a358af1e
AnthonyEssaye <anthonyessaye@outlook.com>
2018-01-03 17:21:38 +0200

    Navigation Dealt with. Fragments ready.

--- app/src/main/java/com/themodernbit/projecthumane/MainActivity.java
+++ app/src/main/java/com/themodernbit/projecthumane/MainActivity.java
@@ -1,8 +1,12 @@
 package com.themodernbit.projecthumane;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,18 +20,22 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private Toolbar toolbar;
+    private String LevelName = "Beginner";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Project Humane");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Work in progress!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -78,19 +86,34 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+
+        Fragment newFragment = null;
+
+
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+         //  newFragment = new LevelFragment();
+          // toolbar.setTitle(LevelName);
 
-        } else if (id == R.id.nav_manage) {
+            Intent goHomeIntent = new Intent(this, MainActivity.class);
+            startActivity(goHomeIntent);
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_camera) {
 
-        } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_settings) {
+
+        }
+
+
+        if(newFragment != null){
+
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_main, newFragment);
+            ft.commit();
 
         }
 
